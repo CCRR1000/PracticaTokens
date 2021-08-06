f660cb1959acb339fec75bcc32ab5ef6c941e672
Michelle Cifuentes <j.micro.0101@gmail.com>
2021-08-05 23:42:37 -0600

    Se creó la clase Principal

---
+++ src/main/java/main/Principal.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import ui.IdentificadorDeTokens;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        IdentificadorDeTokens view = new IdentificadorDeTokens();
+        view.setVisible(true);
+    }
+}
